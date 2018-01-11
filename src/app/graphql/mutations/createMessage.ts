8a8a5f477fd93b705c78d8dd54bb703080b6c129
awsed <edlima@amazon.com>
2018-01-11 18:09:14 +1100

    Initial Commit

--- src/app/graphql/mutations/createMessage.ts
+++ src/app/graphql/mutations/createMessage.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  mutation createMessage($id: ID!, $content: String, $conversationId: ID!, $createdAt: String!) {
-    createMessage(id: $id, content: $content, conversationId: $conversationId, createdAt: $createdAt){
+  mutation createMessage($id: ID!, $content: String, $conversationId: ID!, $createdAt: String!, $sender: String!) {
+    createMessage(id: $id, content: $content, conversationId: $conversationId, createdAt: $createdAt, sender: $sender){
       __typename
       conversationId,
       createdAt,
