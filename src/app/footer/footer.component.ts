56eb3633a690ec4bf9848ec02a0855cd90a9ff08
awsed <edlima@amazon.com>
2018-05-09 21:58:36 +1000

    Minor Login Screen CSS Fixes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   <nav class="navbar fixed-bottom navbar-light bg-light">
     <div class="container">
       <span class="text-muted mx-auto">
-          Powered by <img class="awslogo mx-1" width='35px' src="../assets/img/AWS_logo_RGB.png">
-          <a href="https://aws.amazon.com/appsync" target="_blank">AppSync</a>
+          Powered by <img class="awslogo mx-1" width='35px' src="../assets/img/AWS_logo_RGB.png">AppSync
       </span>
     </div>
   </nav>
