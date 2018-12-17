19cc75395b0290fb98a407fda3739161442c9773
Ed Lima <edlima@amazon.com>
2018-12-18 09:00:17 +1100

    Fix for appsync.service.ts

--- src/app/chat-app/appsync.service.ts
+++ src/app/chat-app/appsync.service.ts
@@ -7,17 +7,22 @@ import { Auth } from 'aws-amplify';
 @Injectable()
 export class AppsyncService {
 
-  hc;
+   // hc;
+   _hc;
 
-  constructor() {
-    const client = new AWSAppSyncClient({
-      url: aws_exports.aws_appsync_graphqlEndpoint,
-      region: aws_exports.aws_project_region,
-      auth: {
-        type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
-        jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken()
-      }
-    });
-    this.hc = client.hydrated;
-  }
+   constructor() {
+     const client = new AWSAppSyncClient({
+       url: aws_exports.aws_appsync_graphqlEndpoint,
+       region: aws_exports.aws_project_region,
+       auth: {
+         type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+         jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken()
+       }
+     });
+     // this.hc = client.hydrated;
+     this._hc = client;
+   }
+   hc() {
+     return this._hc.hydrated();
+   }
 }
